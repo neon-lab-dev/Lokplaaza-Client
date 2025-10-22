8c9bb0d279176a7ba16131f4313f56c271c55fd8
prernabadwane <prernabadwane@gmail.com>
2025-10-22 15:44:11 +0530

    assets added

---
+++ src/assets/index.ts
@@ -0,0 +1,13 @@
+import MITRConsoltancyLogo from "../assets/Images/mitr-consultancy.png";
+
+import cart from "../assets/Icons/cart.svg";
+
+export const IMAGES = {
+  MITRConsoltancyLogo,
+  
+};
+
+export const ICONS = {
+  cart,
+  
+};
