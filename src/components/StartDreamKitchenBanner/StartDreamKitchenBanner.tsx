e387aa908f0c504f7604af6d91342be619aa5cfc
rahulsd380 <rahulsd380@gmail.com>
2025-11-06 12:51:57 +0600

    Completed start dream kitchedn banner

--- src/components/StartDreamKitchenBanner/StartDreamKitchenBanner.tsx
+++ src/components/StartDreamKitchenBanner/StartDreamKitchenBanner.tsx
@@ -7,13 +7,13 @@ const StartDreamKitchenBanner = () => {
   return (
     <div className="bg-primary-10 font-Satoshi py-14">
       <Container>
-        <div className="flex items-center justify-between">
-          <h3 className="text-neutral-20 text-[32px] font-bold leading-9 max-w-[380px]">
+        <div className="flex items-center justify-between gap-6 sm:gap-0">
+          <h3 className="text-neutral-20 text-2xl sm:text-[32px] font-bold leading-9 max-w-[380px]">
             Start Planning Your Dream Kitchen
           </h3>
           <Link
-            href={"/"}
-            className="bg-white hover:bg-success-05/20 transition-all duration-300 size-12 rounded-full flex items-center justify-center"
+            href="/"
+            className="bg-white hover:bg-success-05/20 transition-all duration-300 size-12 rounded-full flex items-center justify-center shrink-0"
           >
             <Image
               src={ICONS.rightArrowRed}
