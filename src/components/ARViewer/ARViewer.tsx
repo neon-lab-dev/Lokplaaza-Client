aa7a1a4e397313e15a416b0f0f009e9cc8479769
prernabadwane <prernabadwane@gmail.com>
2025-12-15 12:39:50 +0530

    usdz changed to dragon

--- src/components/ARViewer/ARViewer.tsx
+++ src/components/ARViewer/ARViewer.tsx
@@ -17,7 +17,7 @@ export default function ProductAR() {
     if (isIOS) {
       const quickLook = document.createElement("a");
       quickLook.rel = "ar";
-      quickLook.href = "/models/test2.usdz";
+      quickLook.href = "/models/testImage.usdz";
       quickLook.click();
     } else {
       const sceneViewerUrl =
